6a8e51609cc5e5684f3861132b84ac9a298c5352
huiruo <2196411859@qq.com>
2022-02-15 08:49:52 +0800

    feat: doc

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -6,7 +6,29 @@ const Login = () => {
   return (
     <div className='login-main'>
       <div className='login-content'>
-        <div className='login-table'>test</div>
+        <div className='login-table'>
+          <div className='table-item'>
+            <label className='input-wrapper'>
+              <input className='login-input' placeholder='请输入邮箱注册' />
+            </label>
+          </div>
+          <div className='table-item'>
+            <label className='input-wrapper'>
+              <input className='login-input' placeholder='请输入注册密码' />
+            </label>
+          </div>
+          <div className='table-item'>
+            <label className='input-wrapper'>
+              <input className='login-input' placeholder='请确认密码' />
+            </label>
+          </div>
+          <div>
+            <button type='submit' >
+              登录
+            </button>
+          </div>
+        </div>
+
         <footer>
           test
         </footer>
