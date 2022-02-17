46d331b446386feb022a8249eda81550651d4c46
huiruo <2196411859@qq.com>
2022-02-18 00:23:50 +0800

    chrom(java): init spring boot use mybatis plus

---
+++ server_java/src/main/java/com/timemanual/TimeManualApplication.java
@@ -0,0 +1,15 @@
+package com.timemanual;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.timemanual.mapper")
+public class TimeManualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManualApplication.class, args);
+    }
+
+}
---
+++ server_java/src/main/java/com/timemanual/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.timemanual.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.timemanual.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
